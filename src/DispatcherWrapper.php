0cf0ea4625d2f844a24fb99a2d5fba4ebaa56b48
qRoC <contact@qroc.pro>
2020-06-02 15:39:30 +0300

    8.0 Symfony 5

--- src/DispatcherWrapper.php
+++ src/DispatcherWrapper.php
@@ -2,7 +2,8 @@
 
 namespace Codeception\PHPUnit;
 
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\Event as ComponentEvent;
+use Symfony\Contracts\EventDispatcher\Event as ContractEvent;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\EventDispatcher\Debug\TraceableEventDispatcherInterface;
 
@@ -12,9 +13,9 @@ trait DispatcherWrapper
      * Compatibility wrapper for dispatcher change between Symfony 4 and 5
      * @param EventDispatcher $dispatcher
      * @param string $eventType
-     * @param Event $eventObject
+     * @param ComponentEvent|ContractEvent $eventObject
      */
-    protected function dispatch(EventDispatcher $dispatcher, $eventType, Event $eventObject)
+    protected function dispatch(EventDispatcher $dispatcher, $eventType, $eventObject)
     {
         //TraceableEventDispatcherInterface was introduced in symfony/event-dispatcher 2.5 and removed in 5.0
         if (!interface_exists(TraceableEventDispatcherInterface::class)) {
